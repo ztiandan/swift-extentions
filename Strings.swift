85c88950152d9edc035acbc71e1c1ef97dc027db
Danny <z.tiandan@gmail.com>
2014-09-16 12:17:55 +0800

    Create Strings.swift

---
+++ Strings.swift
@@ -0,0 +1,29 @@
+extension String {
+    
+    func characterAtIndex(index: Int) -> Character {
+        return Array(self)[index]
+    }
+    
+    
+    // Allows us to use String[index] notation
+    subscript( index: Int ) -> Character {
+        return characterAtIndex(index)
+    }
+    
+    
+    // Allows us to use String[0...2] or String[0..2]
+    subscript( range: Range<Int> ) -> String {
+        
+        var start = advance( startIndex, range.startIndex )
+            var end = advance( startIndex, range.endIndex )
+            
+            return substringWithRange( Range(start:start, end:end) )
+    }
+    
+    
+    func count() -> Int
+    {
+        return countElements( self )
+    }
+    
+}
